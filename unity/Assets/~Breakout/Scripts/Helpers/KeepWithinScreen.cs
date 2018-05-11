d3b099a4218b90c873bfabdaf5b8439ae6911e74
IdaGholami <ida.gholami1@gmail.com>
2018-05-11 15:57:52 +1000

    Finished part one of BreakOut

--- unity/Assets/~Breakout/Scripts/Helpers/KeepWithinScreen.cs
+++ unity/Assets/~Breakout/Scripts/Helpers/KeepWithinScreen.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeepWithinScreen : MonoBehaviour {
+[RequireComponent(typeof(Renderer))]
+public class KeepWithinScreen : MonoBehaviour
+{
+    private Renderer rend;
+    private Camera cam;
+    private Bounds camBounds;
+    private float camWidth, camHeight;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        cam = Camera.main;
+        rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void UpdateCamBounds ()
+    {
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+        camBounds = new Bounds(cam.transform.position, new Vector3(camWidth, camHeight));
 	}
 }
--- unity/Assets/~Breakout/Scripts/Paddle.cs
+++ unity/Assets/~Breakout/Scripts/Paddle.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace Breakout
+{
+    public class Paddle : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+        public Vector2[] directions =
+        {
+            new Vector2(-1, 1),
+            new Vector2(1, 1),
+        };
 
-public class Paddle : MonoBehaviour {
+        private Ball currentBall;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+            currentBall = GetComponentInChildren<Ball>();
+        }
+
+        void Fire()
+        {
+            currentBall.transform.SetParent(null);
+            Vector3 randomDir = directions[Random.Range(0, directions.Length)];
+            currentBall.Fire(randomDir);
+        }
+
+        void CheckInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            Vector3 force = transform.right * inputH;
+            force *= Time.deltaTime;
+            transform.position += force;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckInput();
+            Movement();
+        }
+    }
 }
