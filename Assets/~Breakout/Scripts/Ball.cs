f785617338f763569d9a9408ec864f0e826f31ed
IdaGholami <ida.gholami1@gmail.com>
2018-06-08 15:37:42 +1000

    Worked on RollABall Removed subfolder

---
+++ Assets/~Breakout/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class Ball : MonoBehaviour
+    {
+        public float speed = 5f;
+
+        private Vector3 velocity; 
+        // Use this for initialization
+        public void Fire(Vector3 direction)
+        {
+            velocity = direction * speed;
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            ContactPoint2D contact = other.contacts[0];
+            Vector3 reflect = Vector3.Reflect(velocity, contact.normal);
+            velocity = reflect.normalized * velocity.magnitude; 
+        }
+        // Update is called once per frame
+        private void Update()
+        {
+            transform.position += velocity * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/~Breakout/Scripts/Paddle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class Paddle : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+        public Ball currentBall;
+        public Vector2[] directions = new Vector2[]
+        {
+            new Vector2(-0.5f, 0.5f),
+            new Vector2(0.5f, 0.5f)
+        };
+        // Use this for initialization
+        void Start()
+        {
+            currentBall = GetComponentInChildren<Ball>();
+        }
+
+        void Fire()
+        {
+            currentBall.transform.SetParent(null);
+            Vector3 randomDir = directions[Random.Range(0, directions.Length)];
+            currentBall.Fire(randomDir);
+        }
+        // Update is called once 
+
+        void CheckInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            Vector3 force = transform.right * inputH;
+            force *= movementSpeed;
+            force *= Time.deltaTime;
+            transform.position += force;
+        }
+        void Update()
+        {
+            CheckInput();
+            Movement();
+        }
+    }
+}
