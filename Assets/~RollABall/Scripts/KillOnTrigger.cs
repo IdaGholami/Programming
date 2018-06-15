95c1391022e5e0fba89fd88595168b7cfceb2523
IdaGholami <ida.gholami1@gmail.com>
2018-06-15 15:56:12 +1000

    Worked on assessment

---
+++ Assets/~RollABall/Scripts/KillOnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+namespace RollABall
+{
+    public class KillOnTrigger : MonoBehaviour
+    {
+        void OnParticleCollision()
+        {
+            // Get active scene
+            Scene currentScene = SceneManager.GetActiveScene();
+            // Load the next scene up using buildIndex
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+}
